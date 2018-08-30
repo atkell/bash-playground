94462ddfa5223be4140b2857a50c34034756da25
A.T. Kell <alex@alexkell.com>
2018-08-30 14:39:46 -0400

    introducing composer and magento commands to our script

--- magento2/update_mc.sh
+++ magento2/update_mc.sh
@@ -7,9 +7,9 @@
 echo "Hello, hallo, bonjour, hola, Приве́т..."
 echo "
 This script will attempt to perform 3 tasks:
-[1] WIP: Create a backup of the Magento 2 web root and database
-[2] To-do: Use Composer to update the contents of the eBizMarts mc-magento2 package
-[3] To-do: Use the Magento 2 CLI to run the setup:upgrade, setup:di:compile and cache:flush commands
+[1] (Completed) Create a backup of the Magento 2 web root and database
+[2] WIP: Use Composer to update the contents of the eBizMarts mc-magento2 package
+[3] WIP: Use the Magento 2 CLI to run the setup:upgrade, setup:di:compile and cache:flush commands
 "
 echo "Let's get started by asking for information about the Magento 2 installation\n"
 
@@ -20,3 +20,25 @@ read -p "Magento 2 web root (include trailing slash): " webroot
 tar -cvzf magento2_stage-web-backup-$(date +%Y%m%d).tgz $webroot
 # create a backup of the application database(s), serialize it and store it in the same directory as the files backup
 mysqldump --login-path=magento2_stage magento2_stage > magento2_stage-database-backup-$(date +%Y%m%d).sql
+
+# we need to change our working directory to the magento 2 web root or $webroot
+echo "Switching the working directory to $webroot"
+cd $webroot
+
+# now we need to instruct composer to update either (a) all packages associated within the composer.json OR (b) just the mailchimp/mc-magento2 package
+echo "In order to use composer, we must first change ownership to our system user"
+chown -R freddie:freddie /var/www/magento2
+
+echo "Now we may use composer to update the specific package mailchimp/mc-magento2"
+composer update mailchimp/mc-magento2
+
+echo "And now we should assign ownership of these files back to Apache"
+sudo chown -R www-data:www-data /var/www/magento2
+
+echo "And finally, we may run a few Magento CLI commands"
+php bin/magento setup:upgrade
+php bin/magento setup:di:compile
+php bin/magento cache:flush
+
+echo "Perfecto!"
+
